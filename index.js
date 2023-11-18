b031fd979008c254c1c27a210d6dd68eb4a1895f
Girish keleng <girishkeleng30@gmail.com>
2023-11-19 00:12:45 +0530

    first commit is initialized

---
+++ index.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+// const apiKey = 'x-api-Key';
+
+const API1 = "https://api.spoonacular.com/recipes/complexSearch?" ;
+const API2 = "https://api.spoonacular.com/recipes/";
+const API2_1 = "/information?includeNutrition=false"
+
+const config ={
+    headers: {
+        "x-api-key": "7724091f93404e7595775a14efade20b"
+    },
+}
+
+app.get('/', (req,res)=>{
+    res.render("index.ejs", {content: "kkkkkkkkkkkkkkkkkkkkkk"});
+
+} )
+
+
+let check = false;
+app.post('/get_recipe', async(req, res)=>{
+    const ll = req.body.recipes;
+    check= true;
+    
+    try{
+        const result = await axios.get(API1+"query="+ req.body.recipes,config);
+        
+        res.render("index.ejs", {content: result.data, check:check});
+        console.log(result.data);
+        console.log(ll);
+    } catch(error){
+
+    }
+})
+
+let check2 = false;
+app.post('/get_information', async(req, res)=>{
+
+    check2 = true;
+    const id = req.body.information;
+    try{ 
+        
+        const result = await axios.get(API2+id+API2_1,config);
+        res.render("index.ejs",{data:result.data, check2:check2});
+        console.log(result.data);
+} catch(error){
+
+}
+})
+
+
+
+
+app.listen(port, ()=>{
+    console.log("listening on port 3000");
+})
